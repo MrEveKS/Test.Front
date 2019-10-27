75fdfcacdc81328da4d454f7573a1a84eedbd794
Evgeniy Klimov <eklimov@horse21.net>
2019-10-27 22:27:46 +0300

    Initilize base project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'user/list', pathMatch: 'full' },
+  // { path: 'test-list', component: UserListComponent },
+  { path: 'user', loadChildren: () => import('./components/user/user.module').then(m => m.UserModule) },
+  // { path: 'test', loadChildren: () => import('./components/test/test.module').then(m => m.TestModule)},
+  { path: '**', redirectTo: '/' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule, APP_BASE_HREF } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
+import { UserModule } from './components/user/user.module';
+import { MaterialModule } from './material/material.module';
+
 import { AppComponent } from './app.component';
+import { MaterialNavComponent } from './components/material-nav/material-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MaterialNavComponent,
   ],
   imports: [
+    HttpClientModule,
+    MaterialModule,
+    AppRoutingModule,
+    NgbModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    CommonModule,
+    LayoutModule,
+    UserModule,
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
